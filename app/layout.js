cfc140eb661f138e3084ba44f8b60aebd3f65d22
bacembenhfayehd <bacem.benhfayedh@edu.isetcom.tn>
2025-04-15 21:46:59 +0100

    home page

---
+++ app/layout.js
@@ -0,0 +1,25 @@
+import { Outfit } from "next/font/google";
+import "./globals.css";
+
+import { Toaster } from "react-hot-toast";
+import { AppContextProvider } from "./context/AppContext";
+
+const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
+
+export const metadata = {
+  title: "QuickCart - GreatStack",
+  description: "E-Commerce with Next.js ",
+};
+
+export default function RootLayout({ children }) {
+  return (
+      <html lang="en">
+        <body className={`${outfit.className} antialiased text-gray-700`} >
+          <Toaster />
+          <AppContextProvider>
+            {children}
+          </AppContextProvider>
+        </body>
+      </html>
+  );
+}
