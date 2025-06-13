9b5afb130faf6ca5d2bb3457fe8651120707ba05
bacembenhfayehd <bacem.benhfayedh@edu.isetcom.tn>
2025-06-13 19:27:20 +0100

    add export script

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   output: 'standalone', // Optionnel pour optimiser
+   output: 'export', // Optionnel pour optimiser
   trailingSlash: true,
     images: {
       domains: ['raw.githubusercontent.com'],
