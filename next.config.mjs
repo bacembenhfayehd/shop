bd93ca40e152441674684f42b1e3ae822863e644
bacembenhfayehd <bacem.benhfayedh@edu.isetcom.tn>
2025-06-13 20:32:31 +0100

    Revert "Add TypeScript dependencies"
    This reverts commit 32052edfd4cec4460cae6da454fbb0ff274b86a7.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   output: 'standalone', // Optionnel pour optimiser
-  trailingSlash: true,
     images: {
       domains: ['raw.githubusercontent.com'],
       unoptimized: true,
